2cb83302b1569d49560a422b82ac22f53f907fdb
surgeBinded <ropotdaniel@gmail.com>
2024-12-04 10:15:58 +0100

    add logo and navsearch and shadcn theme

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { VscCode } from "react-icons/vsc";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <VscCode className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 };
 export default Logo;
